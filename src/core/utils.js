c0122b2a828ca136aea152c29a8afdb05e246de9
A1exFox <oxoma91@gmail.com>
2023-10-31 16:41:24 +0300

    feat: reset

--- src/core/utils.js
+++
@@ -1,8 +0,0 @@
-// Pure functions
-export function capitalize(string) {
-  if (typeof string !== 'string') return '';
-  const firstChar = string.charAt(0).toUpperCase();
-  const stringRest = string.slice(1);
-  const resultString = firstChar + stringRest;
-  return resultString;
-}
